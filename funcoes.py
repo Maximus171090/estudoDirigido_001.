e9d5fbd193c0e4d301b6829b60c91795da5e0eb0
maximus171090 <victor17x@gmail.com>
2025-08-24 15:03:11 -0300

    Primeiro código modular com cadastro e listagem

---
+++ funcoes.py
@@ -0,0 +1,30 @@
+#funcoes.py
+
+def exibir_menu():
+    print('\n[1] Cadastrar item [2] Listar itens [0] Sair')
+
+def cadastrar(itens):
+    nome = input('Digite o nome do item:').strip()
+    if not nome:
+        print('✖ Nome não pode se vazio')
+
+        return
+    if nome in itens:
+        print('✖ Item já existe.')
+
+        return
+    itens.append(nome)
+    print('✔ Item Cadastrado!')
+
+
+def listar(itens):
+    if not itens:
+        print('Nenhum item cadastrado.')
+
+    else:
+        for i, nome in enumerate(itens, start=1):
+            
+            print(f'{i}. {nome}')
+
+
+            
